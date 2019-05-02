1445771d473b746f01d62220bd6f4f34b31e9ec8
AlexYu01 <tengalexyu@gmail.com>
2019-05-02 01:23:45 +0000

    Update init to import views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,11 +8,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
-<<<<<<< HEAD
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.reviews import *
-=======
-from api.v1.views.users import *
->>>>>>> c93b24bc1d85abc9a146e81507e7761addf84827
+from api.v1.views.places_reviews import *
