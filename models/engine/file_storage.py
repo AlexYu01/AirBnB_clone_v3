4cc480cf4ff53b462deeeace982e7129c869dd26
AlexYu01 <tengalexyu@gmail.com>
2019-05-02 23:05:23 +0000

    Revert "Minimal changes to files"
    This reverts commit 03bae2f81a6479fb9f1fd051dc4d3be1c8304b34.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -95,6 +95,7 @@ class FileStorage:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
             for key in jo:
+                jo[key]['hash_pw'] = False
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
         except:
             pass
